08de47b7e90b5e3f28d1961d86f352eb1e41eac3
Monica Li <monicali@live.ca>
2021-02-16 17:18:20 -0800

    start home page styles

---
+++ page-templates/template-home.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template Name: Template: Home
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+?>
+
+
+<?php
+get_footer();
