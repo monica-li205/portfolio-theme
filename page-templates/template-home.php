bf51ce54afadbbfcc407264725871064b0786971
Monica Li <monicali@live.ca>
2021-02-17 12:49:22 -0800

    fix home page grid layout

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -14,7 +14,32 @@ defined( 'ABSPATH' ) || exit;
   <div class="title-container">
     <h1 class="page-title">MONICA LI</h1>
   </div>
-  <footer  class="sticky-footer"> 
+  <div class="page-description">
+  </div>
+  <div class="sm-links">
+  </div>
+  <div class="img1">
+  </div>
+  <div class="img2">
+  </div>
+  <div class="img3">
+  </div>
+  <div class="img4">
+  </div>
+  <div class="bottom-nav-buttons">
+    <div class="about-button">
+    </div>
+    <div class="contact-button">
+    </div>
+  </div>
+  </div>
+  <div class="top-nav-buttons">
+    <div class="portfolio-button">
+    </div>
+    <div class="resume-button">
+    </div>
+  </div>
+  <footer class="sticky-footer">
     <?php get_header(); ?>
   </footer>
 </main>
